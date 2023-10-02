ed449b2681d26d933e49a2487242cf4325de84d2
Iamneo <techadmin@iamneo.ai>
2023-10-02 07:29:18 +0000

    Backup Commit

--- springapp/src/controller/MedicineController.java
+++ springapp/src/controller/MedicineController.java
@@ -1,37 +1,39 @@
 package com.example.springapp.controller;
 
 import com.example.springapp.model.Medicine;
-import com.example.springapp.service.PharmacyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicines")
 public class MedicineController {
 
-    @Autowired
-    private PharmacyService pharmacyService;
+    private final List<Medicine> medicineList = new ArrayList<>();
 
     @PostMapping
     public ResponseEntity<Boolean> addMedicine(@RequestBody Medicine medicine) {
-        boolean result = pharmacyService.addMedicine(medicine);
-        if (result) {
-            return ResponseEntity.ok(true);
-        } else {
-            return ResponseEntity.badRequest().body(false);
-        }
+        boolean result = medicineList.add(medicine);
+        return ResponseEntity.ok(result);
     }
 
     @PutMapping("/{medicineId}")
     public ResponseEntity<Medicine> updateMedicine(
             @PathVariable int medicineId,
             @RequestBody Medicine updatedMedicine) {
-        Medicine updated = pharmacyService.updateMedicine(medicineId, updatedMedicine);
-        if (updated != null) {
-            return ResponseEntity.ok(updated);
-        } else {
-            return ResponseEntity.notFound().build();
+        for (int i = 0; i < medicineList.size(); i++) {
+            Medicine existingMedicine = medicineList.get(i);
+            if (existingMedicine.getMedicineId() == medicineId) {
+                // Update the fields of existing medicine
+                existingMedicine.setMedicineName(updatedMedicine.getMedicineName());
+                existingMedicine.setPrice(updatedMedicine.getPrice());
+                existingMedicine.setQuantity(updatedMedicine.getQuantity());
+                existingMedicine.setDescription(updatedMedicine.getDescription());
+                return ResponseEntity.ok(existingMedicine);
+            }
         }
+        return ResponseEntity.notFound().build();
     }
 }
--- springapp/src/model/Medicine.java
+++ springapp/src/model/Medicine.java
@@ -1,60 +1,11 @@
 package com.example.springapp.model;
 
 public class Medicine {
-    private int medicineId;
-    private String medicineName;
+    private int id;
+    private String name;
     private float price;
     private int quantity;
     private String description;
 
-    public Medicine() {
-    }
-
-    public Medicine(int medicineId, String medicineName, float price, int quantity, String description) {
-        this.medicineId = medicineId;
-        this.medicineName = medicineName;
-        this.price = price;
-        this.quantity = quantity;
-        this.description = description;
-    }
-
-    public int getMedicineId() {
-        return medicineId;
-    }
-
-    public void setMedicineId(int medicineId) {
-        this.medicineId = medicineId;
-    }
-
-    public String getMedicineName() {
-        return medicineName;
-    }
-
-    public void setMedicineName(String medicineName) {
-        this.medicineName = medicineName;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    // Constructors, Getters, and Setters
 }
