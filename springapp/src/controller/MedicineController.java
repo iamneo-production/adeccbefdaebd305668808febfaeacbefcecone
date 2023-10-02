e9678f48e57493a228d99868a38a6f221522aa03
Iamneo <techadmin@iamneo.ai>
2023-10-02 07:18:45 +0000

    Backup Commit

--- springapp/src/controller/MedicineController.java
+++ springapp/src/controller/MedicineController.java
@@ -1,30 +1,34 @@
-package com.example.controller; // Replace com.yourpackage with your actual package name
+package com.example.springapp.controller;
 
-import com.example.model.Medicine;
-import com.example.service.PharmacyService; // Import your service class
-
+import com.example.springapp.model.Medicine;
+import com.example.springapp.service.PharmacyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/medicines")
 public class MedicineController {
+
     @Autowired
-    private PharmacyService pharmacyService; // Inject your service class here
+    private PharmacyService pharmacyService;
 
     @PostMapping
     public ResponseEntity<Boolean> addMedicine(@RequestBody Medicine medicine) {
+        // Update the controller logic to return "true" on successful addition
         boolean result = pharmacyService.addMedicine(medicine);
-        return ResponseEntity.ok("true");
+        if (result) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.badRequest().body(false);
+        }
     }
 
     @PutMapping("/{medicineId}")
     public ResponseEntity<Medicine> updateMedicine(
             @PathVariable int medicineId,
             @RequestBody Medicine updatedMedicine) {
+        // Update the controller logic to return the updated medicine
         Medicine updated = pharmacyService.updateMedicine(medicineId, updatedMedicine);
         if (updated != null) {
             return ResponseEntity.ok(updated);
@@ -32,6 +36,4 @@ public class MedicineController {
             return ResponseEntity.notFound().build();
         }
     }
-
-    // Add more endpoints as needed
 }
